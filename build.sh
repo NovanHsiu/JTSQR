987a378b0dcd0e8f3330842c0e2a75e49aa39904
NovanHsiu <s411051@gmail.com>
2014-02-17 16:31:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 hadoop_home=$1
 mtj=$2
 jlablas=$3
 hadoopcore=$(ls $hadoop_home/hadoop-*-core.jar)
 slf4j=$(ls $hadoop_home/lib/slf4j-api-*.jar)
-javac -classpath $hadoopcore:$mtj:$slf4j -d TSQR jtsqr/nthu/scopelab/tsqr/*.java jtsqr/nthu/scopelab/tsqr/matrix/*.java jtsqr/nthu/scopelab/tsqr/math/*.java jtsqr/nthu/scopelab/tsqr/ssvd/*.java jtsqr/nthu/scopelab/tsqr/rs/*.java
+echo "javac -classpath $hadoopcore:$mtj:$slf4j -d TSQR jtsqr/nthu/scopelab/tsqr/*.java jtsqr/nthu/scopelab/tsqr/matrix/*.java jtsqr/nthu/scopelab/tsqr/math/*.java jtsqr/nthu/scopelab/tsqr/ssvd/*.java jtsqr/nthu/scopelab/tsqr/rs/*.java"
