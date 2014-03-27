5e2450d5aa20e183c9af39ec732b94f78362b0c6
NovanHsiu <s411051@gmail.com>
2014-03-27 21:06:47 +0800

    Update cmDenseMatrix.java

--- jtsqr/nthu/scopelab/tsqr/matrix/cmDenseMatrix.java
+++ jtsqr/nthu/scopelab/tsqr/matrix/cmDenseMatrix.java
@@ -20,7 +20,7 @@ public class cmDenseMatrix{
         data = new double[m*n];
 		for(int i=0;i<m;i++)
 		 for(int j=0;j<n;j++)
-		  data[i*n+j] = inmat[i][j];
+		  data[i*n+j] = inmat[j][i];
     }
 	
 	public cmDenseMatrix(double[] inmat,int im,int in) {
